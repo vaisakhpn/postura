1a377fea843bec1723210d02cf85026ca7ccab05
Vaisakh <vaisakhpn78@gmail.com>
2025-10-17 22:16:18 +0530

    Added pages navbar homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className="text-green-500">App</div>;
+  return (
+    <div className="mx-4">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
