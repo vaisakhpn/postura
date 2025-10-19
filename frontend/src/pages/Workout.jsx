f71c4669c0b39a315d17c3ebc8b1c8609eabc71b
Vaisakh <vaisakhpn78@gmail.com>
2025-10-19 21:51:59 +0530

    workout page added

---
+++ frontend/src/pages/Workout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Workout = () => {
+  return (
+    <div className="pt-44 px-8 text-black  min-h-screen">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold mb-4">Workout Dashboard</h1>
+        <p>Here you can view the Tutorials and Ai guided posture correction.</p>
+      </div>
+    </div>
+  );
+};
+export default Workout;
