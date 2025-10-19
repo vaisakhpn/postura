9bbe2a9a8bb541f10492f6d5c59dee7d7dd47582
Vaisakh <vaisakhpn78@gmail.com>
2025-10-19 14:06:01 +0530

    changes made in navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
+import Workout from "./pages/workout";
 
 const App = () => {
   return (
-    <div className="mx-4">
+    <div className="w-screen overflow-x-hidden">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/workout" element={<Workout />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,3 +1,18 @@
+import logo from "./logooo.svg";
+import header_img from "./header_img.png";
+import p3 from "./p3.png";
+import p1 from "./p1.png";
+import p2 from "./p2.png";
+import p4 from "./p4.png";
+
+export const assets = {
+  logo,
+  header_img,
+  p1,
+  p2,
+  p3,
+  p4,
+};
 export const gyms = [
   {
     _id: "gym1",
