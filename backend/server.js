25f36e6d4dea293bad204ee2c771b8f1a83fe79e
Vaisakh <vaisakhpn78@gmail.com>
2025-10-20 12:03:23 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
 import express from "express";
 import cors from "cors";
-
-
-
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectColudinary from "./config/cloudinary.js";
+import gymOwnerRouter from "./routes/gymOwnerRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 // app config
 
 const app = express();
 const port = process.env.PORT || 4000;
-
-
+connectDB();
+connectColudinary();
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 
 // api endpoints
-
+app.use("/api/owner", gymOwnerRouter);
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API WORKING");
