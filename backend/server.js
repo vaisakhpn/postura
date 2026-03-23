a3e2a840031ee860917383471fdce7510e2dbcd3
Vaisakh <vaisakhpn78@gmail.com>
2026-03-23 10:56:29 +0530

    security updated

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,40 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import "dotenv/config";
+
 import connectDB from "./config/mongodb.js";
 import connectColudinary from "./config/cloudinary.js";
 import gymOwnerRouter from "./routes/gymOwnerRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import paymentRouter from "./routes/paymentRoutes.js";
 
-// app config
-
 const app = express();
 const port = process.env.PORT || 4000;
+
 connectDB();
 connectColudinary();
 
-// middlewares
-app.use(express.json());
-app.use(cors());
+app.use(helmet());
 
-// api endpoints
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, please try again later",
+});
+
+app.use(limiter);
+
+app.use(express.json());
+
 app.use("/api/owner", gymOwnerRouter);
 app.use("/api/users", userRouter);
 app.use("/api/payment", paymentRouter);
